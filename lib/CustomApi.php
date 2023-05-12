5314201d81905dfa887518e6a266dbf29ec2515b
dphuang2 <dylan.p.huang@gmail.com>
2023-05-12 11:29:46 -0700

    first draft impl of signature algo

--- lib/CustomApi.php
+++ lib/CustomApi.php
@@ -4,12 +4,34 @@ namespace SnapTrade;
 
 class CustomApi
 {
+
+    private function sort_keys_recursive(&$array)
+    {
+        ksort($array);
+        foreach ($array as &$value) {
+            if (is_array($value)) {
+                $this->sort_keys_recursive($value);
+            }
+        }
+    }
+
     public function beforeSendHook(
         \GuzzleHttp\Psr7\Request $request,
         \SnapTrade\RequestOptions $requestOptions,
         \SnapTrade\Configuration $configuration
     ): \GuzzleHttp\Psr7\Request {
-        return $request;
+        $path = explode($request->getUri()->getHost(), strval($request->getUri()))[1];
+        $query = $request->getUri()->getQuery();
+        $sig_object = [
+            "content" => null,
+            "path" => $path,
+            "query" => $query
+        ];
+        $this->sort_keys_recursive($sig_object);
+        $data = json_encode($sig_object);
+        $hash = hash_hmac('sha256', $data, $configuration->getConsumerKey(), true);
+        $hash = base64_encode($hash);
+        return $request->withHeader("Signature", $hash);
     }
 
     public function beforeCreateRequestHook(

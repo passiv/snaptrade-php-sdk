541f2a0c32f8de945982bbeecb5badbd32279479
Passiv Ops <passiv-ops@passiv.com>
2025-05-08 23:17:28 +0000

    Regenerate SDKs

--- lib/Model/ManualTradeFormWithOptions.php
+++ lib/Model/ManualTradeFormWithOptions.php
@@ -465,7 +465,7 @@ class ManualTradeFormWithOptions implements ModelInterface, ArrayAccess, \JsonSe
     /**
      * Sets symbol
      *
-     * @param string|null $symbol The security's trading ticker symbol. This currently supports stock symbols and Options symbols in the 21 character OCC format. For example \"AAPL  131124C00240000\" represents a call option on AAPL expiring on 2024-11-13 with a strike price of $240. For more information on the OCC format, see [here](https://en.wikipedia.org/wiki/Option_symbol#OCC_format). If 'symbol' is provided, then 'universal_symbol_id' must be 'null'.
+     * @param string|null $symbol The security's trading ticker symbol. This currently supports stock symbols and Options symbols in the 21 character OCC format. For example `AAPL  131124C00240000` represents a call option on AAPL expiring on 2024-11-13 with a strike price of $240. For more information on the OCC format, see [here](https://en.wikipedia.org/wiki/Option_symbol#OCC_format). If 'symbol' is provided, then 'universal_symbol_id' must be 'null'.
      *
      * @return self
      */
--- lib/Model/MlegTradingInstrument.php
+++ lib/Model/MlegTradingInstrument.php
@@ -308,7 +308,7 @@ class MlegTradingInstrument implements ModelInterface, ArrayAccess, \JsonSeriali
     /**
      * Sets symbol
      *
-     * @param string $symbol The security's trading ticker symbol. This currently supports stock symbols and Options symbols in the 21 character OCC format. For example \"AAPL  131124C00240000\" represents a call option on AAPL expiring on 2024-11-13 with a strike price of $240. For more information on the OCC format, see [here](https://en.wikipedia.org/wiki/Option_symbol#OCC_format)
+     * @param string $symbol The security's trading ticker symbol. This currently supports stock symbols and Options symbols in the 21 character OCC format. For example `AAPL  131124C00240000` represents a call option on AAPL expiring on 2024-11-13 with a strike price of $240. For more information on the OCC format, see [here](https://en.wikipedia.org/wiki/Option_symbol#OCC_format)
      *
      * @return self
      */

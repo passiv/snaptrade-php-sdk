8186200b39c7c26223161fd9c30bd1649de5f30c
Passiv Ops <passiv-ops@passiv.com>
2025-12-02 18:35:34 +0000

    Regenerate SDKs

--- lib/Model/TradingInstrument.php
+++ lib/Model/TradingInstrument.php
@@ -336,7 +336,7 @@ class TradingInstrument implements ModelInterface, ArrayAccess, \JsonSerializabl
     /**
      * Sets symbol
      *
-     * @param string $symbol The instrument's trading ticker symbol. This currently supports stock symbols and Options symbols in the 21 character OCC format. For example `AAPL  131124C00240000` represents a call option on AAPL expiring on 2024-11-13 with a strike price of $240. For more information on the OCC format, see [here](https://en.wikipedia.org/wiki/Option_symbol#OCC_format)
+     * @param string $symbol The instrument's trading ticker symbol. This currently supports stock symbols and Options symbols in the 21 character OCC format. For example `AAPL  251114C00240000` represents a call option on AAPL expiring on 2025-11-14 with a strike price of $240. For more information on the OCC format, see [here](https://en.wikipedia.org/wiki/Option_symbol#OCC_format)
      *
      * @return self
      */

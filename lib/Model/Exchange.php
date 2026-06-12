af1f692c1ed7e04b3d1b6b70bb504bbaa7825783
Passiv Ops <passiv-ops@passiv.com>
2026-06-12 13:19:13 +0000

    Regenerate SDKs

--- lib/Model/Exchange.php
+++ lib/Model/Exchange.php
@@ -87,7 +87,7 @@ class Exchange implements ModelInterface, ArrayAccess, \JsonSerializable
     protected static array $openAPINullables = [
         'id' => false,
 		'code' => false,
-		'mic_code' => false,
+		'mic_code' => true,
 		'name' => false,
 		'timezone' => false,
 		'start_time' => false,
@@ -411,7 +411,14 @@ class Exchange implements ModelInterface, ArrayAccess, \JsonSerializable
     {
 
         if (is_null($mic_code)) {
-            throw new \InvalidArgumentException('non-nullable mic_code cannot be null');
+            array_push($this->openAPINullablesSetToNull, 'mic_code');
+        } else {
+            $nullablesSetToNull = $this->getOpenAPINullablesSetToNull();
+            $index = array_search('mic_code', $nullablesSetToNull);
+            if ($index !== FALSE) {
+                unset($nullablesSetToNull[$index]);
+                $this->setOpenAPINullablesSetToNull($nullablesSetToNull);
+            }
         }
 
         $this->container['mic_code'] = $mic_code;
